468c8b26763c0624053314b71e04d2df2aa35339
Ease78 <23438350+Ease78@users.noreply.github.com>
2017-08-30 13:30:48 -0400

    Laser Defender Unity Game
    Worked on it with a part of three people. Added a boss level later on.

---
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+	public GameObject projectile;
+	public float projectileSpeed = 10f;
+	public float health = 150f;
+	public float shotsPerSecond = 0.5f;
+	
+	void Update(){
+		float prob = shotsPerSecond * Time.deltaTime;
+		if(Random.value < prob){
+			Fire ();
+		}
+	}
+	
+	void Fire(){
+		Vector3 offset = new Vector3(0, -1.0f, 0);
+		Vector3 firePos = transform.position + offset;
+		GameObject laser = Instantiate(projectile, firePos, Quaternion.identity) as GameObject;
+		laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0,-projectileSpeed);
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
